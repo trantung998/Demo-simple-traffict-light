1c61eb0d2cda55d2322d20c8d6b1f2d0ea286b8b
trantung998 <melodyinmyheart91@gmail.com>
2015-05-28 22:27:46 +0700

    Hoan thanh tut2 + hide keyboard, set onClick fuction in xml file

--- app/src/main/java/simpletraffictlight/example/tung/demo_simple_traffict_light/MainActivity.java
+++ app/src/main/java/simpletraffictlight/example/tung/demo_simple_traffict_light/MainActivity.java
@@ -1,21 +1,25 @@
 package simpletraffictlight.example.tung.demo_simple_traffict_light;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,9 +50,17 @@ public class MainActivity extends ActionBarActivity implements OnClickListener, 
         mainTextView.setText("Hello word from Main Activity");
         //button
         mainButton = (Button)findViewById(R.id.main_button);
-        mainButton.setOnClickListener(this);
+        //mainButton.setOnClickListener(this);
         //edittext
         mainEditText = (EditText)findViewById(R.id.main_text_editor);
+        mainEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
         mainEditText.clearFocus();
         //list view
         _arrayAdapter = new ArrayAdapter(
@@ -61,11 +73,45 @@ public class MainActivity extends ActionBarActivity implements OnClickListener, 
         mainListView.setOnItemClickListener(this);
         DisplayGreeting();
     }
+
     public void DisplayGreeting(){
         sharedPreferences = getSharedPreferences("pref",MODE_PRIVATE);
-        String name = sharedPreferences.getString("name", "Tung");
+        String name = sharedPreferences.getString("name", "");
 
+        if (name.length() > 0) {
         Toast.makeText(this,name,Toast.LENGTH_LONG).show();
+        } else {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle("Hello");
+            dialog.setMessage("What your name?");
+
+            final EditText editText = new EditText(this);
+            dialog.setView(editText);
+
+            //make ok button
+            dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String name = editText.getText().toString();
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("name", name);
+                    editor.commit();
+
+                    Toast.makeText(getApplicationContext(), "Welcome, " + name + "!", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            dialog.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+
+            dialog.show();
+        }
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -110,6 +156,16 @@ public class MainActivity extends ActionBarActivity implements OnClickListener, 
     public void onItemClick(AdapterView<?   > parent, View view, int position, long id) {
 
         Log.d("", position + ": " + _arrayNameList.get(position));
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public void OnTextviewClick(View v) {
+        Log.d("", "test");
+        Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG).show();
     }
 
 }
